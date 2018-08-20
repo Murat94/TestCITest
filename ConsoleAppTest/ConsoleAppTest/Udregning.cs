cf43fef75883fce639772fd9777e0f2eb7ae4342
mura0149 <31722880+mura0149@users.noreply.github.com>
2018-08-20 10:45:49 +0200

    Opsætning

---
+++ ConsoleAppTest/ConsoleAppTest/Udregning.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppTest
+{
+    public class Udregning
+    {
+        public int add(int x, int y)
+        {
+            return x + y;
+        }
+
+        public int minus(int x, int y)
+        {
+            return x - y;
+        }
+    }
+}
---
+++ ConsoleAppTest/ConsoleAppTestTests/UdregningTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleAppTest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppTest.Tests
+{
+    [TestClass()]
+    public class UdregningTests
+    {
+        [TestMethod()]
+        public void addTest()
+        {
+            Udregning udregning = new Udregning();
+
+            int expected = udregning.add(5, 5);
+
+            Assert.AreEqual(10, expected);
+        }
+    }
+}
